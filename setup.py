c69173ca1148c160b26cccb34ce211e2f8d378d5
node3112 <node3113@gmail.com>
2023-03-08 12:29:05 +0000

    Fixed setup.py.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name="kirjava",
-        version="0.1.3",
+        version="0.1.4",
         description="A Java bytecode library for Python.",
         # classifiers=[
         #     "Programming Language :: Python",
@@ -23,10 +23,13 @@ if __name__ == "__main__":
             "kirjava.analysis.ir",
             "kirjava.classfile",
             "kirjava.classfile.attributes",
-            "kirjava.classfile.instructions",
+            "kirjava.instructions",
+            "kirjava.instructions.ir",
+            "kirjava.instructions.jvm",
             "kirjava.jarfile",
             "kirjava.skeleton",
             "kirjava.skeleton.reconstruct",
+            "kirjava.skeleton.skeletons",
             "kirjava.types",
             "kirjava.verifier",
         ],
@@ -41,19 +44,23 @@ if __name__ == "__main__":
             # "src/kirjava/skeleton/*.pyx",
             # "src/kirjava/skeleton/reconstruct/*.pyx",
             # "src/kirjava/types/*.pyx",
-            # "src/kirjava/verifier/*.pyx",
+            "src/kirjava/verifier/*.pyx",
         ], build_dir="build"),
         package_dir={
             "": "src",
         },
-        data_files=[
-            ("kirjava/skeleton/skeletons", [
-                "src/kirjava/skeleton/skeletons/skeletons_j8.json",
-                "src/kirjava/skeleton/skeletons/skeletons_j11.json",
-                "src/kirjava/skeleton/skeletons/skeletons_j17.json",
-                "src/kirjava/skeleton/skeletons/skeletons_j19.json",
-            ]),
-        ],
+        package_data={
+            "kirjava.skeleton.skeletons": ["*.json"],
+        },
+        include_package_data=True,
+        # data_files=[  # FIXME
+        #     ("kirjava/skeleton/skeletons", [
+        #         "src/kirjava/skeleton/skeletons/skeletons_j8.json",
+        #         "src/kirjava/skeleton/skeletons/skeletons_j11.json",
+        #         "src/kirjava/skeleton/skeletons/skeletons_j17.json",
+        #         "src/kirjava/skeleton/skeletons/skeletons_j19.json",
+        #     ]),
+        # ],
         requires=[
             "Cython",
         ],
