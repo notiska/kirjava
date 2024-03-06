a43405547711f29d606ce6d07c955eae4cb8c20a
notiska <node3113@gmail.com>
2024-03-06 12:20:03 +0000

    PyPI friendly setup.py.

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 
+from pathlib import Path
 from setuptools import setup
 
 
 if __name__ == "__main__":
     setup(
-        name="kirjava",
+        name="kirjava-jvm",
         version="0.1.5",
         description="A Java bytecode library for Python.",
+        long_description=(Path(__file__).parent / "README.md").read_text(),
+        long_description_content_type="text/markdown",
         # classifiers=[
         #     "Programming Language :: Python",
         # ],
         author="node3112 (Iska)",
         author_email="node3112@protonmail.com",
         license="GPL-3.0",
-        url="https://github.com/node3112/kirjava",
+        url="https://github.com/notiska/kirjava",
         packages=[
             "kirjava",
             "kirjava.abc",
@@ -29,16 +32,5 @@ if __name__ == "__main__":
         package_dir={
             "": "src",
         },
-        package_data={
-            "kirjava.skeleton.skeletons": ["*.json"],
-        },
-        include_package_data=True,
-        # data_files=[  # FIXME
-        #     ("kirjava/skeleton/skeletons", [
-        #         "src/kirjava/skeleton/skeletons/skeletons_j8.json",
-        #         "src/kirjava/skeleton/skeletons/skeletons_j11.json",
-        #         "src/kirjava/skeleton/skeletons/skeletons_j17.json",
-        #         "src/kirjava/skeleton/skeletons/skeletons_j19.json",
-        #     ]),
-        # ],
+        keywords=["java", "jvm", "bytecode", "assembler", "disassembler"],
     )
