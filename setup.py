992bf6e7508f3828217830b7751f5b6f367127d1
notiska <node3113@gmail.com>
2024-05-31 15:16:28 +0100

    Updated to `0.1.6`, fixed pip install issues on Windows.

--- setup.py
+++ setup.py
@@ -4,18 +4,34 @@ from pathlib import Path
 from setuptools import setup
 
 
+# This is my first PyPI package, just trying my best lol.
 if __name__ == "__main__":
     setup(
         name="kirjava-jvm",
-        version="0.1.5",
+        version="0.1.6",
         description="A Java bytecode library for Python.",
         long_description=(Path(__file__).parent / "README.md").read_text(),
         long_description_content_type="text/markdown",
+        classifiers=[  # Somewhat modelled after https://github.com/ninia/jep, thanks :).
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Topic :: File Formats",  # Maybe?
+            "Topic :: Software Development",
+            "Topic :: Software Development :: Assemblers",
+            "Topic :: Software Development :: Code Generators",
+            "Topic :: Software Development :: Disassemblers",
+            "Topic :: Software Development :: Libraries :: Java Libraries",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3.10",  # TODO: Would be nice to support eariler versions.
+            "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
+        ],
         # classifiers=[
         #     "Programming Language :: Python",
-        # ],
-        author="node3112 (Iska)",
-        author_email="node3112@protonmail.com",
+        # ],        author="node3112 (Iska)",
+        author_email="node3113@gmail.com",
         license="GPL-3.0",
         url="https://github.com/notiska/kirjava",
         packages=[
